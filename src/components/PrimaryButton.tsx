c2b90525ebe096b8189c6f11c40eee365691cadf
ttakuma17 <tk21.dek93k@gmail.com>
2021-11-03 19:15:51 +0900

    commit before set default css themes

--- src/components/PrimaryButton.tsx
+++ src/components/PrimaryButton.tsx
@@ -3,18 +3,20 @@ import React, { memo, ReactNode, VFC } from 'react';
 
 type Props = {
   children: ReactNode;
-  onClick: () => void;
+  // onClick: () => void;
 };
 
 // ボタンに実装する処理
 // ボタンのロード中は機能をdisabledにする
 // ボタンの色をどうするか?使う場所によって色を変えたいので、一旦はここで指定はしないことにします
 
+//  Buttonに指定した、onClick={onClick} を一時的に削除
+
 export const PrimaryButton: VFC<Props> = memo((props) => {
-  const { children, onClick } = props;
+  const { children } = props;
   return (
     <>
-      <Button m={2} _hover={{ opacity: 0.8 }} onClick={onClick}>
+      <Button m={2} _hover={{ opacity: 0.8 }}>
         {children}
       </Button>
     </>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import React, { memo, VFC } from 'react';
 import { Header } from '../components/Header';
+import { MemoItem } from '../components/MemoItem';
 
 export const Home: VFC = memo(() => {
   return (
     <>
       <Header />
       <p>Homeページです</p>
+      <MemoItem />
     </>
   );
 });
