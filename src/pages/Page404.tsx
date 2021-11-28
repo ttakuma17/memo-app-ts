77589d14980fa7df85c25b716ad9c5d009d460b5
ttakuma17 <tk21.dek93k@gmail.com>
2021-11-28 15:10:10 +0900

    update 404ページ

--- src/pages/Page404.tsx
+++ src/pages/Page404.tsx
@@ -1,12 +1,39 @@
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
+} from '@chakra-ui/alert';
+import { CloseButton } from '@chakra-ui/close-button';
+import { Box, Center, Spacer } from '@chakra-ui/layout';
 import React, { memo, VFC } from 'react';
+import { useHistory } from 'react-router';
 import { Header } from '../components/Header';
+import { PrimaryButton } from '../components/PrimaryButton';
 
 export const Page404: VFC = memo(() => {
+  const history = useHistory();
   return (
     <>
       <Header />
-      <h2>404ページ</h2>
-      <p>指定されたページは存在しません</p>
+      <Box p={4}>
+        <Alert status="error">
+          <AlertIcon />
+          <AlertTitle mr={2}>お探しのページは見つかりませんでした。</AlertTitle>
+          <AlertDescription pr={4}>
+            以下のログインページへアクセスしてください。
+          </AlertDescription>
+        </Alert>
+      </Box>
+
+      <Center>
+        <PrimaryButton
+          onClick={() => {
+            history.push('/');
+          }}>
+          ログインページ
+        </PrimaryButton>
+      </Center>
     </>
   );
 });
